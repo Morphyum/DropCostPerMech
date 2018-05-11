8cc9d2b214cf539ff87612dca3487d5ad5474952
Morphyum <zion@c-schwabe.de>
2018-05-11 18:15:47 +0200

    *transfered cost from start to end of contract

--- DropCostPerMech/DropCostPerMech/Patch.cs
+++ DropCostPerMech/DropCostPerMech/Patch.cs
@@ -14,8 +14,7 @@ namespace DropCostPerMech {
 
         static void Postfix(AAR_ContractObjectivesWidget __instance) {
             try {
-                MissionObjectiveResult missionObjectiveResult = new MissionObjectiveResult("Drop Cost: 100000 C-Bills", "7facf07a-626d-4a3b-a1ec-b29a35ff1ac0", false, true, ObjectiveStatus.Succeeded, false);
-
+                MissionObjectiveResult missionObjectiveResult = new MissionObjectiveResult("Drop Cost: "+ Mathf.FloorToInt(LanceHeaderWidget_RefreshLanceInfo.cbill) + " C-Bills", "7facf07a-626d-4a3b-a1ec-b29a35ff1ac0", false, true, ObjectiveStatus.Succeeded, false);
                 ReflectionHelper.InvokePrivateMethode(__instance, "AddObjective", new object[] { missionObjectiveResult });
             }
             catch (Exception e) {
@@ -29,7 +28,7 @@ namespace DropCostPerMech {
 
         static void Postfix(Contract __instance) {
             try {
-                int newMoneyResults = Mathf.FloorToInt(__instance.MoneyResults - 100000f);
+                int newMoneyResults = Mathf.FloorToInt(__instance.MoneyResults - LanceHeaderWidget_RefreshLanceInfo.cbill);
                 ReflectionHelper.InvokePrivateMethode(__instance, "set_MoneyResults", new object[] { newMoneyResults });
             }
             catch (Exception e) {
@@ -40,14 +39,14 @@ namespace DropCostPerMech {
 
     [HarmonyPatch(typeof(LanceHeaderWidget), "RefreshLanceInfo")]
     public static class LanceHeaderWidget_RefreshLanceInfo {
-
+        public static float cbill;
         static void Postfix(LanceHeaderWidget __instance, List<MechDef> mechs) {
             try {
                 LanceConfiguratorPanel LC = (LanceConfiguratorPanel)ReflectionHelper.GetPrivateField(__instance, "LC");
                 if (LC.IsSimGame) {
                     float num2 = 0f;
                     int lanceTonnageRating = SimGameBattleSimulator.GetLanceTonnageRating(LC.sim, mechs, out num2);
-                    float cbill = 0f;
+                    cbill = 0f;
                     foreach (MechDef def in mechs) {
                         cbill += (float)def.Description.Cost * 0.001f;
                     }
