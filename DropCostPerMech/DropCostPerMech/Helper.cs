b4c54ab01084be9a4e36a9242a96e06b947c9294
Morphyum <zion@c-schwabe.de>
2018-06-09 18:06:31 +0200

    *added save state

--- DropCostPerMech/DropCostPerMech/Helper.cs
+++ DropCostPerMech/DropCostPerMech/Helper.cs
@@ -3,6 +3,14 @@ using System;
 using System.IO;
 
 namespace DropCostPerMech {
+    public class SaveFields {
+        public float cbill = 0;
+
+        public SaveFields(float cbill) {
+            this.cbill = cbill;
+        }
+    }
+
     public class Helper {
 
         public static Settings LoadSettings() {
@@ -15,6 +23,39 @@ namespace DropCostPerMech {
             catch (Exception ex) {
                 Logger.LogError(ex);
                 return null;
+            }
+        }
+
+        public static void SaveState(string instanceGUID, DateTime saveTime) {
+            try {
+                int unixTimestamp = (int)(saveTime.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+                string filePath = "mods/DropCostPerMech/saves/" + instanceGUID + "-" + unixTimestamp + ".json";
+                (new FileInfo(filePath)).Directory.Create();
+                using (StreamWriter writer = new StreamWriter(filePath, true)) {
+                    SaveFields fields = new SaveFields(Fields.cbill);
+                    string json = JsonConvert.SerializeObject(fields);
+                    writer.Write(json);
+                }
+            }
+            catch (Exception ex) {
+                Logger.LogError(ex);
+            }
+        }
+
+        public static void LoadState(string instanceGUID, DateTime saveTime) {
+            try {
+                int unixTimestamp = (int)(saveTime.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+                string filePath = "mods/DropCostPerMech/saves/" + instanceGUID + "-" + unixTimestamp + ".json";
+                if (File.Exists(filePath)) {
+                    using (StreamReader r = new StreamReader(filePath)) {
+                        string json = r.ReadToEnd();
+                        SaveFields save = JsonConvert.DeserializeObject<SaveFields>(json);
+                        Fields.cbill = save.cbill;
+                    }
+                }
+            }
+            catch (Exception ex) {
+                Logger.LogError(ex);
             }
         }
     }
--- DropCostPerMech/DropCostPerMech/Patch.cs
+++ DropCostPerMech/DropCostPerMech/Patch.cs
@@ -1,5 +1,7 @@
 ﻿using BattleTech;
 using BattleTech.Framework;
+using BattleTech.Save;
+using BattleTech.Save.SaveGameStructure;
 using BattleTech.UI;
 using Harmony;
 using System;
@@ -8,6 +10,20 @@ using TMPro;
 using UnityEngine;
 
 namespace DropCostPerMech {
+    [HarmonyPatch(typeof(GameInstanceSave))]
+    [HarmonyPatch(new Type[] { typeof(GameInstance), typeof(SaveReason) })]
+    public static class GameInstanceSave_Constructor_Patch {
+        static void Postfix(GameInstanceSave __instance) {
+            Helper.SaveState(__instance.InstanceGUID, __instance.SaveTime);
+        }
+    }
+
+    [HarmonyPatch(typeof(GameInstance), "Load")]
+    public static class GameInstance_Load_Patch {
+        static void Prefix(GameInstanceSave save) {
+            Helper.LoadState(save.InstanceGUID, save.SaveTime);
+        }
+    }
 
     [HarmonyPatch(typeof(AAR_ContractObjectivesWidget), "FillInObjectives")]
     public static class AAR_ContractObjectivesWidget_FillInObjectives {
